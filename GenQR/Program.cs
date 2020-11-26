9800cf8cace50f56b4ab8d35b912bcf90e90f1ff
dotyc@civicinc.com <dotyc@civicinc.com>
2020-11-25 23:37:13 -0600

    feat: test qr

---
+++ GenQR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+
+namespace GenQR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save("qrtest.png", ImageFormat.Png);
+        }
+    }
+}
